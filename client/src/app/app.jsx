a1f96440366161ef3f12d9e79a965c2e9d472efd
Dave Coble <d.coble@f5.com>
2020-12-17 13:36:06 -0800

    annoying

---
+++ client/src/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SampleComponent} from "./components/sample-components/sample-component";
+
+function App() {
+  return (
+      <div>
+        <SampleComponent/>
+      </div>
+  );
+}
+
+export default App;
---
+++ client/src/app/components/sample-components/sample-component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import axios from 'axios'
+
+function callServer() {
+    axios.get('http://dice.coblefriends.com:8000/test', {
+        params: {
+            table: 'sample',
+            rows: [
+                {name: 'twerpy'},
+                {name: 'burpy'},
+            ],
+        },
+    }).then((response) => {
+        console.log(JSON.stringify(response.data));
+    });
+}
+
+export function SampleComponent() {
+    return (
+        <div>
+            { callServer() }
+            This is a sample component
+        </div>
+    );
+}
+
