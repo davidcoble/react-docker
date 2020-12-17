4c86237e380e0b4620a0c02cbf14221d846e5e73
Dave Coble <d.coble@f5.com>
2020-12-17 13:42:21 -0800

    annoyed

--- server/index.js
+++ server/index.js
@@ -5,28 +5,29 @@ const mysql = require('mysql');
 const app = express();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST_IP,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST_IP,
+    port: 3307
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
 });
 
 app.use(cors());
 
 app.listen(process.env.REACT_APP_SERVER_PORT, () => {
-  console.log(`YYYYYYYYYYYYYY App server now listening on port ${process.env.REACT_APP_SERVER_PORT}`);
+    console.log(`YYYYYYYYYYYYYY App server now listening on port ${process.env.REACT_APP_SERVER_PORT}`);
 });
 
 app.get('/test', (req, res) => {
-  const table = req.query;
-  const params = req.params;
-  console.log("table = " + JSON.stringify(table,null, 2));
-  pool.query(`insert into sample values (0, 'webby')`)
-  pool.query(`select * from ${table.table}`, (err, results) => {
-    if (err) {
-      return res.send(err);
-    } else {
-      return res.send(results);
-    }
-  });
+    const table = req.query;
+    const params = req.params;
+    console.log("table = " + JSON.stringify(table,null, 2));
+    pool.query(`insert into sample values (0, 'webby')`)
+    pool.query(`select * from ${table.table}`, (err, results) => {
+	if (err) {
+	    return res.send(err);
+	} else {
+	    return res.send(results);
+	}
+    });
 });
